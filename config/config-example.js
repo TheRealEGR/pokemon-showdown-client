1ad4f390830b44165f6961a4552cd5bd4db89baf
TheRealEGR <razvanrazvan502@gmail.com>
2024-12-23 19:09:09 +0100

    a

--- config/config-example.js
+++ config/config-example.js
@@ -19,8 +19,8 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
-	port: 443,
+	host: 'localhost',
+	port: 8000,
 	httpport: 8000,
 	altport: 80,
 	registered: true
